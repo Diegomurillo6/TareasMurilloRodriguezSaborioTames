e356b5d627d3a5abdaac70ff11900992b24269f5
Diego <diegomuqa06@gmail.com>
2025-03-04 23:08:49 -0600

    Codigo con errores detectables con flake8

---
+++ Tarea 1/codigo_para_corregir.py
@@ -0,0 +1,23 @@
+#definición de funciones
+
+def validarMes():
+    numValidos="0123456789"
+    if int(mes)<1 or int(mes)>12:
+        return "invalido"
+    else:
+        return "valido"
+
+
+def multiplicar():
+    i=0
+    valorA=int(input("Ingrese el primer valor:"))
+    valorB=int(input("Ingrese el segundo valor: "))
+    valorC=int(input("Ingrese el tercer valor: "))
+    while i==0:
+        if valorB<valorC:
+            multiplicar=(valorA*valorB)
+            i+=1
+            print(multiplicar)
+        else:
+            return "Ingrese un tercer valor que sea mayor al segundo"
+    return
